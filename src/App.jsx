61e051fdb70af9711e39c635b6a61c3dcfe5becc
mordaz <mariaordazr15@gmail.com>
2025-08-17 23:17:00 -0400

    Cambiando estilo de presentación de nuevo

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,13 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div className="app-container">
-      <Header />
+      <Header/>
       <main className="hero-section">
         <h1 className="hero-text">
-          <span className="hero-line">Hi. I'm Maria Ordaz.</span>
-          <span className="hero-line">A Software Developer.</span>
+          <span className="hero-line">Hi. I'm Maria Ordaz.<br/>A Software Developer.</span>
         </h1>
       </main>
-      <Footer />
+      <Footer/>
     </div>
   );
 }
